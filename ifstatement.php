889291fa4e40b439ccf26d0a3916a28e9b1271fa
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-10-14 15:27:18 -0500

    Updated If Statement PHP
    Added a form

--- ifstatement.php
+++ ifstatement.php
@@ -7,11 +7,24 @@
 </head>
 <body>
 <?php require "header.php";?>
+       
     <?php
         //An if statement will carry out an action based on the value of a variable
-        echo "<h1>If-Else Statement</h1>";
+        echo "<h1>If Statement</h1>";
+        echo "<hr/>";
 
-        $grade = 5;
+        echo "
+        <form method='post'>
+        Type your grade below:<br>
+        <input type='text' id='gradeEntered' name='gradeEntered'><br>
+        <input type='submit' id='submit' name='submit'>
+        </form>";
+
+        $gradeEntered = 0;
+        if(isset($_REQUEST['submit'])){
+        $gradeEntered = $_REQUEST['gradeEntered'];
+
+        $grade = $gradeEntered;
 
         if($grade >= 50){
             echo "<h2 style='color: green'>YOU HAVE PASSED</h2>";       
@@ -22,23 +35,28 @@
          echo "<h2>Grade: $grade</h2>";
         
          echo "<hr/>";
+        }
+    ?>
 
-         $grade = "C";
+    <?php
+        echo "<h1>If-Else Statement</h1>";
+        $grade1 = "C";
          //If-Else If-Else
-         if ($grade == "A"){
+         if ($grade1 == "A"){
              echo "<h2 style='color: green'>YOU ARE A SUPERSTAR!!!!!</h2>";
          }
-         elseif ($grade  == "B"){
+         elseif ($grade1  == "B"){
             echo "<h2 style='color: orange'>YOU DID WELL!!!!!</h2>";
         }
-        elseif ($grade  == "C"){
+        elseif ($grade1  == "C"){
             echo "<h2 style='color: blue'>YOU DID GOOD!!!!!</h2>";
         }
         else{
             echo "<h2 style='color: red'>YOU HAVE FAILED!!!!!</h2>";
         }
-        echo "<h2>Grade: $grade</h2>";
+        echo "<h2>Grade: $grade1</h2>";
     ?>
+
 <?php include "footer.php";?>
 </body>
 </html>
