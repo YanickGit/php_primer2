2c2357811f715d3216112f9ad5b9c245b62f1e4f
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-10-14 14:34:27 -0500

    Update Footer
    Update Footer

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 echo "<br>";
 echo "<hr>";
 date_default_timezone_set('America/Bogota');
-$jamaica_date_time = date("yy/m/d, g:i a", time());
+$jm_date_time = date("yy/m/d, g:i a", time());
 $year = date("y");
-echo "<p>Copyright &copy; 2019-$year | Yanick Levy | 1500174645 | Assignment 1 - Module 1 | Current Date & Time: $jamaica</p>";
+echo "<p>Copyright &copy; 2019-$year | Yanick Levy | 1500174645 | Assignment 1 - Module 1 | Current Date & Time: $jm_date_time</p>";
 ?>
 
 
